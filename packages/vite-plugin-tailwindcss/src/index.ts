e56d2005468c08401051faaa50c1668fdf0fdaa4
Nicolas Sutter <contact.sutter.nicolas@gmail.com>
2023-01-13 11:10:00 +0100

    feat: add support for options

--- packages/vite-plugin-tailwindcss/src/index.ts
+++ packages/vite-plugin-tailwindcss/src/index.ts
@@ -3,11 +3,14 @@ import fs from 'node:fs/promises'
 import type { Plugin } from 'vite'
 import { build } from 'esbuild'
 import fg from 'fast-glob'
+import type { Options as AutoprefixerOptions } from 'autoprefixer'
+
+const OUTDIR = path.resolve(process.cwd(), '.vite_plugin_tailwindcss')
+const OUTFILE = path.resolve(process.cwd(), OUTDIR, '_tailwind.config.cjs')
+const OUT_CONFIG_FILE = path.resolve(OUTDIR, 'tailwind.config.cjs')
 
 async function generate({ watch, config }: { watch: boolean; config: string }) {
-  const dir = path.resolve(process.cwd(), '.vite')
-
-  await build({
+  const result = await build({
     format: 'cjs',
     // Bundle imports together
     bundle: true,
@@ -15,7 +18,7 @@ async function generate({ watch, config }: { watch: boolean; config: string }) {
     packages: 'external',
     write: true,
     entryPoints: [config],
-    outfile: path.resolve(process.cwd(), dir, '_tailwind.config.cjs'),
+    outfile: OUTFILE,
     watch,
     plugins: [
       {
@@ -23,7 +26,7 @@ async function generate({ watch, config }: { watch: boolean; config: string }) {
         setup(build) {
           build.onEnd(() => {
             fs.writeFile(
-              path.resolve(dir, 'tailwind.config.cjs'),
+              OUT_CONFIG_FILE,
               [
                 `const TW_CONFIG = require('./_tailwind.config.cjs');`,
                 `const config = 'default' in TW_CONFIG ? TW_CONFIG.default : TW_CONFIG`,
@@ -35,6 +38,8 @@ async function generate({ watch, config }: { watch: boolean; config: string }) {
       },
     ],
   })
+
+  return result
 }
 
 async function plugin() {
@@ -46,21 +51,71 @@ async function plugin() {
 
   if (!TW_CONFIG_FILE) {
     throw new Error(
-      'No Tailwind CSS config file was found. Please make sure you have placed a `tailwind.config.{js,cjs,ts,mjs}` at the root of your project.',
+      'No Tailwind CSS config file was found. Please make sure that you have placed a `tailwind.config.{js,cjs,ts,mjs}` at the root of your project.',
     )
   }
 
-  generate({
+  return generate({
     watch: process.env.NODE_ENV === 'development',
     config: TW_CONFIG_FILE,
   })
 }
+export type Options = {
+  /**
+   * @see https://tailwindcss.com/docs/using-with-preprocessors#nesting
+   */
+  nesting?: 'postcss-nested' | 'postcss-nesting'
+  /**
+   * Options to pass to autoprefixer
+   *
+   * @see https://github.com/postcss/autoprefixer#options
+   */
+  autoprefixerOptions?: AutoprefixerOptions
+}
 
-export function vitePluginTailwindcss(_config = {}): Plugin {
-  plugin()
+export const defaultConfig: Options = {}
+
+export function vitePluginTailwindcss(config: Options = defaultConfig): Plugin {
+  const pluginResult = plugin()
 
   return {
     name: 'vite-plugin-tailwindcss',
+
+    async config() {
+      const [
+        { default: TailwindCSS },
+        { default: TailwindCSSNesting },
+        { default: autoprefixer },
+      ] = await Promise.all([
+        import('tailwindcss'),
+        import(
+          // @ts-expect-error untyped
+          'tailwindcss/nesting/index.js'
+        ),
+        import('autoprefixer'),
+      ])
+
+      const plugins = [
+        config.nesting ? TailwindCSSNesting(config.nesting) : undefined,
+        TailwindCSS({
+          config: OUT_CONFIG_FILE,
+        }),
+        autoprefixer(config.autoprefixerOptions ?? {}),
+      ].filter((item) => item !== undefined)
+
+      return {
+        css: {
+          postcss: {
+            plugins,
+          },
+        },
+      }
+    },
+
+    async closeWatcher() {
+      const buildResult = await pluginResult
+      buildResult?.stop && buildResult.stop()
+    },
   }
 }
 
